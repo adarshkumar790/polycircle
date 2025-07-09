873e203c2ca842199db1c63785a9a68d6d29cd8a
adarshkumar790 <adarshnit420@gmail.com>
2025-07-09 12:12:34 +0530

    polycircle

--- src/GraphQuery/query.ts
+++ src/GraphQuery/query.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const GRAPH_API_URL = "https://api.studio.thegraph.com/query/112968/lltpolycircle/version/latest";
+const GRAPH_API_URL = "https://api.studio.thegraph.com/query/112968/polycircle/version/latest";
 
 const ENDPOINTS = [
-  "https://api.studio.thegraph.com/query/112968/lltpolycircle/version/latest",
+  "https://api.studio.thegraph.com/query/112968/polycircle/version/latest",
 ];
 
 
