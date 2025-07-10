901e040a3e0439c066d07d5fc50e853d1c6c7656
adarshkumar790 <adarshnit420@gmail.com>
2025-07-10 17:28:38 +0530

    contract address update

--- src/GraphQuery/query.ts
+++ src/GraphQuery/query.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const GRAPH_API_URL = "https://api.studio.thegraph.com/query/112968/lltpolycircle/version/latest";
+const GRAPH_API_URL = "https://api.studio.thegraph.com/query/112968/lltpolycircle/v0.0.2";
 
 const ENDPOINTS = [
-  "https://api.studio.thegraph.com/query/112968/lltpolycircle/version/latest",
+  "https://api.studio.thegraph.com/query/112968/lltpolycircle/v0.0.2",
 ];
 
 
