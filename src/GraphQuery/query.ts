ff0eeefbfe553a805c712b73b7ab37e12f5d8f0c
adarshkumar790 <adarshnit420@gmail.com>
2025-07-13 15:41:23 +0530

    update conttract address

--- src/GraphQuery/query.ts
+++ src/GraphQuery/query.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const GRAPH_API_URL = "https://api.studio.thegraph.com/query/112968/polycricle/version/latest";
+const GRAPH_API_URL = "https://api.studio.thegraph.com/query/112968/polycircles/version/latest";
 
 const ENDPOINTS = [
-  "https://api.studio.thegraph.com/query/112968/polycricle/version/latest",
+  "https://api.studio.thegraph.com/query/112968/polycircles/version/latest",
 ];
 
 
